458408e5769785bb886fe02035e1ea3e57c703b1
liuchuang <chuang.liu@bqjr.cn>
2017-09-25 11:42:50 +0800

    Redis常量

---
+++ src/main/java/com/liuis/redis/config/RedisConfig.java
@@ -0,0 +1,14 @@
+package com.liuis.redis.config;
+/**
+ * Redis常量
+ * @author chuang.liu
+ *
+ */
+public interface RedisConfig {
+	
+	public static final String REDIS_HOST = "10.83.20.59";
+	
+    public static final int REDIS_PORT = 6379;
+    
+    
+}
